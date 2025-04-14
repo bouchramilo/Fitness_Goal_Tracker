b92bf4b1d64971ed55c415c21943233e5b13d50c
Miloudy bouchra <miloudybouchra01@gmail.com>
2025-04-14 12:04:11 +0100

    try 3 : résoudre le problème de déploiment

--- fitness_goal/vite.config.js
+++ fitness_goal/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Fitness_Goal_Tracker/fitness_goal/',
+  base: '/Fitness_Goal_Tracker/',
   css: {
     postcss: './postcss.config.cjs' // Spécifiez explicitement le chemin
   }
