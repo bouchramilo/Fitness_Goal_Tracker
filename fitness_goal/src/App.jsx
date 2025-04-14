614bcaeee019a39d18c6b1168db1e3e045abb27f
Miloudy bouchra <miloudybouchra01@gmail.com>
2025-04-14 12:30:18 +0100

    try 5 : résoudre le problème de déploiment

--- fitness_goal/src/App.jsx
+++ fitness_goal/src/App.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
+// import { HashRouter as Router } from "react-router-dom";
+
 import Home from "./pages/Home";
 import Dashboard from "./pages/Dashboard";
 import NotFound from "./pages/404";
